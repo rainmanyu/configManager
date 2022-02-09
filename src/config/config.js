8a15bac723b729711f23df6c4878e9817be913bc
rainman.yu <rainman.yu@everymatrix.com>
2022-02-09 11:43:51 +0000

    simplify the page.

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-let server_base = 'http://localhost:9889'
+let server_base = 'http://10.2.47.17:9888'
 export let g_server_sites_url = server_base + '/sites'
 export let g_server_site_url = server_base + '/site/'
