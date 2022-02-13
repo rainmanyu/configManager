8494bd3fc4917b7bf4902491446b8f09a6c91412
rainman.yu <rainman.yu@everymatrix.com>
2022-02-13 13:40:49 +0000

    remove useless files

--- src/main.js
+++ src/main.js
@@ -2,17 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import 'element-ui/lib/theme-chalk/index.css';
-import './style/index.css';
-import axios from "axios";
-
 import ElementUI from 'element-ui';
 
-
-Vue.config.productionTip = false
 Vue.use(ElementUI);
-
-
-Vue.prototype.$axios=axios;
 Vue.prototype.$bus=new Vue();
 
 new Vue({
